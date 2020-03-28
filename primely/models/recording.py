b8e481c5b72431d70b3d27b862b15aa0a8e5ab00
dev-yoshiki <ben.nakagawa01@gmail.com>
2020-03-28 18:17:50 +0900

    removed all mongo related snippets

--- primely/models/recording.py
+++ primely/models/recording.py
@@ -1,15 +1,10 @@
 import collections
 import json
 import os
-import pprint
 import pathlib
 
-import pymongo
+import pprint
 
-from pymongo import MongoClient
-
-CLIENT_HOST = 'mongodb://localhost:27017/'
-CLIENT_NAME = 'test_database'
 JSON_DIR_PATH = 'data/output/json'
 
 
@@ -41,43 +36,12 @@ class JsonModel(object):
             json_file_path = pathlib.Path(JSON_DIR_PATH, filename).with_suffix('.json')
         return json_file_path
 
-class MongoModel(object):
-    def __init__(self, db):
-        self.db = db
 
-    def get_mongo_profile(self):
-        """Define DB. Check if db is active.
-        :rtype: Boolen
-        """
-        try:
-            client = MongoClient(host=CLIENT_HOST, serverSelectionTimeoutMS=100)
-            client.server_info()
-            self.db = client[CLIENT_NAME]
-            return True
-        except pymongo.errors.ServerSelectionTimeoutError as error:
-            return False
+class RecordingModel(JsonModel):
+    """Definition of class that generates ranking model"""
 
-
-class RecordingModel(JsonModel, MongoModel):
-    """Definition of class that generates ranking model to write to MongoDB"""
-
-    def __init__(self, filename, mongo_status, json_file=None, db=None):
+    def __init__(self, filename, json_file=None, db=None):
         JsonModel.__init__(self, filename, json_file)
-        MongoModel.__init__(self, db)
-        if mongo_status:
-            self.get_mongo_profile()
-
-    def record_data_to_mongo(self, dict_data):
-        """Store dict_data to MongoDB"""
-
-        # Insert data to DB
-        stack = dict_data
-        db_stacks = self.db.stacks
-        stack_id = db_stacks.insert_one(stack).inserted_id
-
-        # Print to see the inserted data
-        # print(stack_id, type(stack_id))
-        # pp.pprint(db_stacks.find_one({'_id': stack_id}))
 
     def record_data_in_json(self, stack):
         """Export dict_data in json format"""
