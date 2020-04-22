605c623e38ec7b2099674cca85d9d32800501036
dev-yoshiki <ben.nakagawa01@gmail.com>
2020-04-22 14:29:07 +0900

    Tweeked context

--- tests/context.py
+++ tests/context.py
@@ -1,16 +1,10 @@
 import os
 import sys
+
 print(sys.path, '\n')
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 print(sys.path, '\n')
 
-
-# from pathlib import Path
-# Path(__file__).resolve().parent.parent
-
-# import sample
 from primely.models import pdf_reader
 
 
-
-# from models import pdf_reader
