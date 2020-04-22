0f59039570f77e7531058d6fffa632512409a5ca
dev-yoshiki <ben.nakagawa01@gmail.com>
2020-04-22 13:01:54 +0900

    Isolated queueing class

--- primely/models/pdf_reader.py
+++ primely/models/pdf_reader.py
@@ -48,28 +48,6 @@ class PdfReader(object):
         return 'success'
 
 
-class InputQueue(object):
-    def __init__(self, base_dir=None, all_files=None, pdf_files=None):
-        if not base_dir:
-            base_dir = utils.get_base_dir_path(__file__)
-        self.base_dir = base_dir
-        self.all_files = all_files
-        self.pdf_files = pdf_files
-    
-    def load_pdf_filenames(self, all_files=list):
-        """List all pdf files in designated directory"""
-        all_files = []
-        pdf_full_dir_path = pathlib.Path(self.base_dir, PDF_DIR_PATH)
-        self.pdf_files = os.listdir(pdf_full_dir_path)
-
-        # Extract filename without suffix
-        for item in os.listdir(pdf_full_dir_path):
-            filename, _ = os.path.splitext(item)
-            all_files.append(filename)
-        self.all_files = sorted(all_files)
-        return self.all_files
-
-
 def main():
     # inputQueue = InputQueue()
     # filenames = inputQueue.load_pdf_filenames()
