ae6d861f335ba3c00a47ba90caeabbb385464f81
dev-yoshiki <ben.nakagawa01@gmail.com>
2020-05-16 13:19:16 +0900

    Applied file upload

--- frontend_api.py
+++ frontend_api.py
@@ -1,72 +1,29 @@
+import os
 import sqlite3
 
+from flask import flash
 from flask import Flask
 from flask import g
+from flask import redirect
 from flask import render_template
 from flask import request
 from flask import Response
+from flask import send_from_directory
+from flask import url_for
+from werkzeug.utils import secure_filename
 
-# from primely.controller import controller
 from primely.controller import controller
 
-# DATABASE = 'test_sqlite.db'
+UPLOAD_FOLDER = 'data/input'
+ALLOWED_EXTENSIONS = {'pdf'}
+
 app = Flask(__name__)
-
-
-# def get_db():
-#     db = getattr(g, '_database', None)
-#     if db is None:
-#         db = g._database = sqlite3.connect(DATABASE)
-#     return db
-
-# @app.teardown_appcontext
-# def close_connection(exception):
-#     db = getattr(g, '_database', None)
-#     if db is not None:
-#         db.close()
-
-# @app.route('/app', methods=['POST', 'PUT', 'DELETE'])
-# @app.route('/app/<name>', methods=['GET'])
-# def employee(name=None):
-#     db = get_db()
-#     curs = db.cursor()
-#     curs.execute(
-#         'CREATE TABLE employees ('
-#         'emp_no DECIMAL(7) PRIMARY KEY,'
-#         'emp_name VARCHAR(100) NOT NULL,'
-#         'dept_name VARCHAR(100));'
-#     )
-
-#     name = request.values.get('emp_no', emp_no)
-#     if request.method == 'GET':
-#         curs.execute('SELECT * FROM employees WHERE emp_no = "{}"'. format(emp_no))
-#         employee = curs.fetchone()
-#         if not employee:
-#             return "No", 404
-#         emp_no, emp_name, dept_name = employee
-#         return '{}:{}:{}'.format(emp_no, emp_name, dept_name), 200
-
-#     if request.method == 'POST':
-#         curs.execute('INSERT INTO employees() values("{}")'.format(name))
-#         db.commit()
-#         return 'created{}'.format(name), 201
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
 def hello_world():
     return 'top'
-
-@app.route('/hello/')
-@app.route('/hello/<username>')
-def hello_world2(username=None):
-    # return 'hello world! {}'.format(username)
-    return render_template('hello.html', username=username)
-
-@app.route('/post', methods=['POST', 'PUT', 'DELETE'])
-def show_post():
-    # return str(request.values)
-    return str(request.values['username'])
 
 @app.route('/convert', methods=['GET'])
 def run_conversion():
@@ -76,6 +33,42 @@ def run_conversion():
             return "No", 404
 
         return 'conversion success', 200
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/interface', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 def main():
     app.debug = True
