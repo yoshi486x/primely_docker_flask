a8ef8ade6e67d157f9c293ac0c7281ba383d1648
dev-yoshiki <ben.nakagawa01@gmail.com>
2020-05-22 16:22:08 +0900

    Removed cache of relocated folders

--- data/README.md
+++
@@ -1,4 +0,0 @@
-# Data
-
-1. Download your paycheck PDF files.
-2. Save them to `data/input/`, respectively.
--- tests/archive/_pdf_reader_test.py
+++
@@ -1,6 +0,0 @@
-import pprint as pp
-
-from models import pdf_reader
-
-pdf = pdf_reader.PdfReader()
-pdf.convert_pdf_to_txt()
