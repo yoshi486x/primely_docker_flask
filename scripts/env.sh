45a75a8520e50ab7eda9d4621b4d74a9cacd1cc4
dev-yoshiki <ben.nakagawa01@gmail.com>
2020-05-26 15:11:34 +0900

    Renamed setup.sh

--- scripts/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export FLASK_APP="flaskr/flask_app.py"
-echo "success: export FLASK_APP=$FLASK_APP"
-
-export FLASK_RUN_PORT=80
-echo "success: export FLASK_RUN_PORT=$FLASK_RUN_PORT"
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-export PRIMELY_ROOT="${PWD}/primelyr"
-echo "success: export PRIMELY_ROOT=$PRIMELY_ROOT"
 
-mkdir $PRIMELY_ROOT/data/input
+export FLASK_APP="flaskr/flask_app.py"
+echo "success: export FLASK_APP=$FLASK_APP"
+
+export FLASK_RUN_PORT=80
+echo "success: export FLASK_RUN_PORT=$FLASK_RUN_PORT"
